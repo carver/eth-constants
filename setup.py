e87472606d845c576cfd2defa5b54e4b226b2695
Jason Carver <ut96caarrs@snkmail.com>
2018-05-15 12:51:30 -0700

    Bump version: 0.1.0-alpha.0 → 0.1.0-alpha.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras_require['dev'] = (
 setup(
     name='eth-constants',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.0',
+    version='0.1.0-alpha.1',
     description="""eth-constants: Easy access to a variety of Ethereum constants in Python""",
     long_description_markdown_filename='README.md',
     author='Jason Carver',
